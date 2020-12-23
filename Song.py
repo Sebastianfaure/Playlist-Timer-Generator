f1ed98b57535017b4e31d9e953f2478f736e2c9a
Sebastianfaure <54461558+Sebastianfaure@users.noreply.github.com>
2020-12-23 14:59:07 -0500

    Create Song.py

---
+++ Song.py
@@ -0,0 +1,7 @@
+#create song class to store the data in
+class Song:
+    def __init__(self, artists, duration_ms, id, name):
+        self.artists = artists #3
+        self.duration_ms = duration_ms #5
+        self.id = id #8
+        self.name = name #14
